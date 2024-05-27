0ce611f89252d28bd2970d9772c39b9fda89a5f3
Legatario <l.phillipe.d@gmail.com>
2024-05-27 15:12:39 -0300

    dando estilos simples aos componentes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,6 +3,7 @@ import React from "react";
 const Button = ({Text, onClick, Type="button"}) =>{
     return (
         <button
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded custom-button"
         type={Type}
         onClick={onClick}
         >{Text}
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,6 +3,7 @@ import React from "react";
 const Input = ({type, placeholder, value, onChange}) =>{
     return (
         <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline custom-input"
         value={value}
         onChange={onChange}
         type={type}

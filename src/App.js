afe658ebaa4fbe5b8a2306bdf64c79e2b664fd4c
Legatario <l.phillipe.d@gmail.com>
2024-05-27 13:44:30 -0300

    criando hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import RoutesApp from './routes/routes';
+import { AuthProvider } from './contexts/auth';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RoutesApp />
-    </>
+    </AuthProvider>
   );
 }
 
